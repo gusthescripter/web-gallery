3dd5017a5ed24143fc36bd4c7b1464bb5bd40b10
gus <gus.spencer@protonmail.com>
2022-08-10 12:19:59 -0700

    fetch tester

--- src/components/Post.js
+++ src/components/Post.js
@@ -3,11 +3,15 @@ import React, { useState, useEffect } from 'react'
 
 export default function Post() {
 	
-	const [postData, setPost] = useState(null)
+	const [postData, setPost] = useState(null);
 	
-	useEffect() => {
-		.fetch()
-	}
+	useEffect(() => {
+		fetch('http://gusspencer.tech/bk/wp-json/wp/v2/media')
+			.then((response) => response.json())
+			.then((json) => {
+				console.log(json);
+			});
+	})
 	
 	return
 }
