43ffbe29b88d70b1ffe4b42551bdb446a8c34d3c
gus <gus.spencer@protonmail.com>
2022-08-11 09:24:54 -0700

    pulling from api and rendering

--- src/components/Post.js
+++ src/components/Post.js
@@ -6,12 +6,23 @@ export default function Post() {
 	const [postData, setPost] = useState(null);
 	
 	useEffect(() => {
-		fetch('http://gusspencer.tech/bk/wp-json/wp/v2/media')
-			.then((response) => response.json())
-			.then((json) => {
-				console.log(json);
-			});
-	})
-	
-	return
+		fetch('http://gusspencer.tech/bk/wp-json/wp/v2/posts')
+			.then(res => {
+				return res.json();
+			})
+			.then(data => {
+				console.log(data);
+				setPost(data);
+			})
+			.catch(console.error);
+			
+	}, []);
+	return (
+		<main>
+			<section>
+				<h1>test</h1>
+				{postData && postData[0].title.rendered}
+			</section>
+		</main>
+	)
 }
